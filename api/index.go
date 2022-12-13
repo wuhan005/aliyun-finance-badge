a741862538e08d1562b5777a0649ddbde71fb1dc
E99p1ant <i@github.red>
2022-12-13 23:01:43 +0800

    balance: move to index

--- api/index.go
+++ api/index.go
@@ -8,8 +8,13 @@ import (
 	"bytes"
 	"io"
 	"net/http"
+	"os"
 
+	"github.com/aliyun/alibaba-cloud-sdk-go/sdk"
+	"github.com/aliyun/alibaba-cloud-sdk-go/sdk/auth/credentials"
+	"github.com/aliyun/alibaba-cloud-sdk-go/services/bssopenapi"
 	"github.com/narqo/go-badge"
+	"github.com/pkg/errors"
 )
 
 func Handler(w http.ResponseWriter, r *http.Request) {
@@ -32,4 +37,25 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	_, _ = io.Copy(w, bytes.NewReader(badgeBytes))
+}
+
+func getBalance() (string, string, error) {
+	accessKeyID := os.Getenv("ALIYUN_ACCESS_KEY_ID")
+	accessKeySecret := os.Getenv("ALIYUN_ACCESS_KEY_SECRET")
+	regionID := os.Getenv("ALIYUN_REGION_ID")
+
+	config := sdk.NewConfig()
+	credential := credentials.NewAccessKeyCredential(accessKeyID, accessKeySecret)
+	client, err := bssopenapi.NewClientWithOptions(regionID, config, credential)
+	if err != nil {
+		return "", "", errors.Wrap(err, "create client")
+	}
+
+	request := bssopenapi.CreateQueryAccountBalanceRequest()
+	request.Scheme = "https"
+	response, err := client.QueryAccountBalance(request)
+	if err != nil {
+		return "", "", errors.Wrap(err, "query account balance")
+	}
+	return response.Data.Currency, response.Data.AvailableAmount, nil
 }
